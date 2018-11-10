cd1ba22dc58bcbe95b3bc22e7c6d998b64ecf107
riita10069 <ryota10069.tech5.jizi@gmail.com>
2018-11-10 15:42:57 +0900

    commit

--- js/fullscreen.js
+++ js/fullscreen.js
@@ -1,3 +1,14 @@
+var getDevice = (function(){
+    var ua = navigator.userAgent;
+    if(ua.indexOf('iPhone') > 0 || ua.indexOf('iPod') > 0 || ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0){
+        return 'sp';
+    }else if(ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0){
+        return 'tab';
+    }else{
+        return 'other';
+    }
+})();
+
 var winH = $(window).height();
 $('.box').outerHeight(winH);
  
