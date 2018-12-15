7cbff9170a1b424601adf00ffc2d049741cdf38b
riita10069 <ryota10069.tech5.jizi@gmail.com>
2018-12-15 18:12:36 +0900

    after all

--- top_image1.php
+++
@@ -1,9 +0,0 @@
-<?php
-$html=<<<EOF
-    <video autoplay loop muted type="video/mp4" class="fitImg" src="images/math_chalkbord.mp4" alt="">
-        <div class="box__centered">
-            <div class="box__contents">
-            </div>
-        </div>
-    </video>
-EOF;
